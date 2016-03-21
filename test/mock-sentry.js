bd95b65e4f72a116f4f07c892b87e53d30b381e5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-21 16:33:23 -0400

    chore(test): confirming the version

--- test/mock-sentry.js
+++ test/mock-sentry.js
@@ -2,6 +2,9 @@
 
 const nock = require('nock')
 const zlib = require('zlib')
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
 nock('http://localhost')
   .post('/api/11/store/')
   .reply(200, function (uri, body) {
@@ -9,7 +12,10 @@ nock('http://localhost')
     const buffer = new Buffer(body, 'base64')
     zlib.unzip(buffer, {}, function (err, msg) {
       const errorMessage = JSON.parse(msg.toString('utf8'))
-      console.log(errorMessage.message)
-      console.assert(/this is a test/.test(errorMessage.message), 'expected error')
+      console.log('received error message', errorMessage.message)
+      la(/this is a test/.test(errorMessage.message),
+        'wrong error message', errorMessage)
+      la(errorMessage.release === '0.0.0-semantic-release',
+        'invalid release, should be package version', errorMessage.release)
     })
   })
