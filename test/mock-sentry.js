cd5c86d92ac333fc570537a3f744ce5e122fe7c9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-29 13:40:57 -0400

    chore(test): tested string conversion for #5

--- test/mock-sentry.js
+++ test/mock-sentry.js
@@ -5,6 +5,7 @@ const zlib = require('zlib')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 
+// first error
 nock('http://localhost')
   .post('/api/11/store/')
   .reply(200, function (uri, body) {
@@ -16,6 +17,24 @@ nock('http://localhost')
       console.log('release / version', errorMessage.release)
 
       la(/this is a test/.test(errorMessage.message),
+        'wrong error message', errorMessage)
+      la(errorMessage.release === '0.0.0-semantic-release',
+        'invalid release, should be package version', errorMessage.release)
+    })
+  })
+
+// second error
+nock('http://localhost')
+  .post('/api/11/store/')
+  .reply(200, function (uri, body) {
+    console.log('received 2nd mock sentry request', uri)
+    const buffer = new Buffer(body, 'base64')
+    zlib.unzip(buffer, function (err, msg) {
+      const errorMessage = JSON.parse(msg.toString('utf8'))
+      console.log('received error message 2', errorMessage.message)
+      console.log('release / version 2', errorMessage.release)
+
+      la(/this is a test 2/.test(errorMessage.message),
         'wrong error message', errorMessage)
       la(errorMessage.release === '0.0.0-semantic-release',
         'invalid release, should be package version', errorMessage.release)
--- test/test.js
+++ test/test.js
@@ -8,3 +8,5 @@ const report = reporter()
 la(is.fn(report), 'expected to get report function', report)
 
 report(new Error('this is a test error'))
+
+report(new Error('this is a test 2 error'))
